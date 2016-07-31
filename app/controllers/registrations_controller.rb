8fd00c5483a58cd14ff674c011dbf83e3afbdfb9
neil patel <neil@myindianweddingapp.com>
2016-07-31 10:52:20 +0100

    Added extra user attributes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,10 +3,10 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :username)
+    params.require(:user).permit(:email, :password, :password_confirmation, :username, :current_password)
   end
 
   def account_update_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :username, :current_password)
+    params.require(:user).permit(:email, :password, :password_confirmation, :username, :current_password, :age, :bio, :gender)
   end
 end
