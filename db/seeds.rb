2b194e58570704130096fa1598eb8372321f0d80
neil patel <neil@myindianweddingapp.com>
2016-08-01 12:03:56 +0100

    Added database seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "neil87", email: "neil87@gmail.com", password: "neil12", password_confirmation: "neil12")
+User.create(username: "neil88", email: "neil88@gmail.com", password: "neil12", password_confirmation: "neil12")
+User.create(username: "neil89", email: "neil89@gmail.com", password: "neil12", password_confirmation: "neil12")
+User.create(username: "neil90", email: "neil90@gmail.com", password: "neil12", password_confirmation: "neil12")
+User.create(username: "neil91", email: "neil91@gmail.com", password: "neil12", password_confirmation: "neil12")
+
+p "Test users created"
